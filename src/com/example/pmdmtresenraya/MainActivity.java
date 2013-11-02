64353425d31f457ec306b2f26ff76ef69c6f0ccd
MartaLL <m_a_r_t_a_1_9@hotmail.com>
2013-11-02 13:17:52 +0100

    First

---
+++ src/com/example/pmdmtresenraya/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.pmdmtresenraya;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Button btnJugar=(Button) findViewById(R.id.btnJugar);
+		btnJugar.setOnClickListener(new OnClickListener(){
+			public void onClick(View arg0) {
+				Intent intent=new Intent(MainActivity.this,Juego.class);
+				startActivity(intent);
+			}
+		});
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return false;
+	}
+
+}
